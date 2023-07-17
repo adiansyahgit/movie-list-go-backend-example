bd8f6aa805ab29c69e791b7f4134aedac218f832
Adiansyah <mail.adiansyah@gmail.com>
2023-07-17 10:19:14 +0700

    achieve: implement routes using go chi chi

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -18,7 +18,7 @@ func main() {
 
 	log.Println("Running in port", port)
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
