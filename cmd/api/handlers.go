f9152d17eb3dd1d0ef26986650d67df12db3fdbf
Adiansyah <mail.adiansyah@gmail.com>
2023-07-18 20:53:38 +0700

    achieve: implement error json response

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -13,6 +13,7 @@ func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
 	movies, err := app.DB.AllMovies()
 	if err != nil {
 		fmt.Println(err)
+		app.errorJSON(w, err)
 		return
 	}
 
